cbe40c89658669d0a2c96354a14d69b503ab1604
tpinckney <teresa@visionshiftcatalyst.com>
2025-07-19 21:36:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// JavaScript for logic model builder functionality including:
+// - Add/remove items
+// - Shareable link
+// - Export as image/PDF/Excel (to be implemented)
+
+document.getElementById('addBtn').addEventListener('click', () => {
+  // Add item logic
+});
